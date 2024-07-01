3f119a4a141526de26ba721cf57673bd08657555
sergejgolubcov <golubcov.s@mail.ru>
2024-07-02 00:18:38 +0300

    Функции перенесли в класс ООП

--- women/views.py
+++ women/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+class Women():
+    def index(request):
+        return HttpResponse("Стариница приложения women")
+
+    def categories(request):
+        return HttpResponse("<h1>Категории товаров</h1>")
