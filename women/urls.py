6b680a9105d0253cf157e7e0c344e86ac564a653
sergejgolubcov <golubcov.s@mail.ru>
2024-07-02 23:11:45 +0300

    Добавили динамический URL

--- women/urls.py
+++ women/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.Women.index),
-    path("cat/", views.Women.categories),
+    path("cat/<int:cats_id>/", views.Women.categories),
+    path("num/<slug:nums_id>/", views.Women.random_int)
 ]
--- women/views.py
+++ women/views.py
@@ -1,9 +1,24 @@
+import random
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 class Women():
+
+
+
     def index(request):
         return HttpResponse("Стариница приложения women")
 
-    def categories(request):
-        return HttpResponse("<h1>Категории товаров</h1>")
+    def categories(request, cats_id):
+        return HttpResponse(f"<h1>Категории товаров c id {cats_id}</h1>")
+
+    def random_int(request, nums_id):
+        start = 0
+        stop = 100
+        rand = random.randint(start, stop)
+        return HttpResponse(f"<h2> Выбрали случайное число от {start} до {stop}, "
+                            f"и получили число {rand}<br>"
+                            f"slug {nums_id}")
+
+
