ab9963c671e328bcc97b2300ca22f10a77138a91
sergejgolubcov <golubcov.s@mail.ru>
2024-07-11 22:20:49 +0300

    Добавили метод cars

--- women/urls.py
+++ women/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.Women.index),
     path("cat/<int:cats_id>/", views.Women.categories),
-    path("num/<slug:nums_id>/", views.Women.random_int)
+    path("num/<slug:nums_id>/", views.Women.random_int),
+    path("cars/", views.Women.cars),
 ]
--- women/views.py
+++ women/views.py
@@ -1,11 +1,8 @@
 import random
-
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 
 class Women():
-
-
 
     def index(request):
         return HttpResponse("Стариница приложения women")
@@ -17,12 +14,17 @@ class Women():
         start = 0
         stop = 100
         rand = random.randint(start, stop)
+        if rand > 90:
+            raise Http404()
         return HttpResponse(f"<h2> Выбрали случайное число от {start} до {stop}, "
                             f"и получили число {rand}<br>"
                             f"slug {nums_id}")
 
+    def cars(request):
+        return HttpResponse(f"Продолжаем обучение")
+
     def page_not_found(request, exception):
-        return HttpResponseNotFound(f"Ваша страница не найдена")
+        return HttpResponseNotFound("Ваша страница не найдена")
 
 
 
