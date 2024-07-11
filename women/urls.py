739e5a0032c3360a2aa6f4feef2e8c69a4d42ad1
sergejgolubcov <golubcov.s@mail.ru>
2024-07-11 22:52:30 +0300

    добавили название url

--- women/urls.py
+++ women/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Women.index),
+    path("", views.Women.index, name='home'),
     path("cat/<int:cats_id>/", views.Women.categories),
     path("num/<slug:nums_id>/", views.Women.random_int),
     path("cars/<int:cars_id>/", views.Women.cars),
