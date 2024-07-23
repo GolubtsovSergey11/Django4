2591ed87310caa0098a6312149ccb19ef3e323ab
sergejgolubcov <golubcov.s@mail.ru>
2024-07-24 00:14:10 +0300

    создал первую модель с полями title content time_create time_update is_poblished

--- women/models.py
+++ women/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Womens(models.Model):
+
+    """Создание первой модели """
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_poblished = models.BooleanField(default=True)
+
