d221cfed7f7ceaeecd1def35ecec5fe10e45c7c0
sergejgolubcov <golubcov.s@mail.ru>
2024-07-23 22:51:07 +0300

    подсветили что это простой тэг

--- women/templatetags/women_tags.py
+++ women/templatetags/women_tags.py
@@ -6,11 +6,12 @@ import women.views as views
 #Создаем экземпляр класса Library (нужен для регистрации наших тэгов)
 register = template.Library()
 
-
+#Простой тэг
 @register.simple_tag(name="getcat")
 def get_categories():
     return views.cats_db
 
+#Простой тэг
 @register.simple_tag(name='sportsmen')
 def sportsmen():
     return views.world_sport
